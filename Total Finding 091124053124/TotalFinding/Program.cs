51cf16f5c1827b3cf588933ca5b004dc66477107
Ender1202 <106911891+Ender1202@users.noreply.github.com>
2024-12-07 11:18:12 +0530

    Add files via upload

---
+++ Total Finding 091124053124/TotalFinding/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TotalFinding
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Implement your code here
+            int sum = 0;
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the last number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine(num2);
+            }
+            else 
+            {
+                for (int i = num1; i <= num2; i++) 
+                {
+                    sum += i;
+                    Console.WriteLine(sum);
+                }
+            }
+        }
+    }
+}
